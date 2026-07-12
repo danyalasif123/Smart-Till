aeeeeceb66f9180c354acf3750e5817297a8e9c1
Danyal Asif <danyalasi797@gmail.com>
2026-07-12 15:41:14 +0100

    ..

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
-const express = require("express");
+const app = require("./app");
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });

6e1deef5fe104829d21a630844a73092df92c53c
Danyal Asif <danyalasi797@gmail.com>
2026-08-05 14:39:37 +0100

    ..

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.error("❌ Database Error:", error.message);
-    process.exit(1);
+    console.error(error);
   }
 };
 
